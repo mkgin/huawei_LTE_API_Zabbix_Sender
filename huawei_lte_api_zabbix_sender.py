7661e3766921e8164030bf9facebfcb7b2edb9e2
Mike G <mkgin@users.noreply.github.com>
2022-08-30 17:52:38 +0300

    minor pylint cleaning

--- huawei_lte_api_zabbix_sender.py
+++ huawei_lte_api_zabbix_sender.py
@@ -5,7 +5,7 @@
 # - monitor, store last and only report changes for some parameters send data to Zabbix.
 # - collect timestamps
 #
-# - for some items, send timestamp , mark what was last sent and send when 
+# - for some items, send timestamp , mark what was last sent and send when
 # - tested with a 4G-LTE modem ( Huawei B535-232) so I may have missed some parameters
 #   the would be interesting to 5G (or 3G)
 #
@@ -54,8 +54,11 @@ def load_config():
         config.update(yaml.safe_load(open(configfile_own)))
     else:
         print("Missing",configfile_own )
-    global modem_url, zabbix_sender_setting,monitored_hostname, minimum_polling_interval,log_level, do_it_once, do_zabbix_send
+
+    global modem_url, zabbix_sender_setting,monitored_hostname
+    global minimum_polling_interval,log_level, do_it_once, do_zabbix_send
     global zabbix_send_failed_time_max, zabbix_send_failed_items_max
+
     modem_url=config['modem_url']
     zabbix_sender_setting=config['zabbix_sender_setting']
     monitored_hostname = config['monitored_hostname']
@@ -64,29 +67,30 @@ def load_config():
     do_zabbix_send = config['do_zabbix_send']
     log_level = config['log_level']
     logging.basicConfig(level=logging.INFO) #FIX this
-    
+
     zabbix_send_failed_time_max  = 900 # TODO add to default config
     zabbix_send_failed_items_max = 500 # TODO add to default config
 
 def save_zabbix_packet_to_disk(object):
     logging.warning(f'TEST: save_zabbix_packet_to_disk: NOT IMPLEMENTED YET' )
-    
-    
 
 #todo
 #parsed = "txpower"
-key_prefix = 'huawei.lte' # name for the start of the zabbix key... rest is the api endpoint and api key (for the value)
+key_prefix = 'huawei.lte' # name for the start of the zabbix key...
+#rest is the api endpoint and api key (for the value)
 
 # endpoint 'device.signal'
 # what to parse "PPusch:7dBm PPucch:-3dBm PSrs:-40dBm PPrach:7dBm"
 
 # always interesting sent every polling interval
 always_interesting = ['rsrq', 'rsrp', 'rssi', 'sinr', 'txpower' ]
-# changes_interesting are sent when changed or when the last result is older than the last one to be sent.
-# when there is a change in value, the previous value is sent also (with timestamp of the previous interval
+# changes_interesting are sent when changed or when the last result is older
+# than the last one to be sent. When there is a change in value, the previous
+# value is sent also (with timestamp of the previous interval
 # the idea of this is to make graphs look nicer.
 changes_interesting_10min =  ['pci' ,'cell_id','band','nei_cellid']
-changes_interesting_1hour =  ['ims','tac','mode', "rrc_status",'plmn','lteulfreq', 'ltedlfreq', 'enodeb_id', 'ulbandwidth','dlbandwidth' ]
+changes_interesting_1hour =  ['ims','tac','mode', "rrc_status",'plmn','lteulfreq', 'ltedlfreq',
+                              'enodeb_id', 'ulbandwidth','dlbandwidth' ]
 interesting = always_interesting +  changes_interesting_10min + changes_interesting_1hour
 
 # will be determined from the smallest interval in the API configuration
@@ -103,7 +107,7 @@ def main():
     epoch_time_start = int(time.time())
     zabbix_send_failed_time = 0
     with Connection(modem_url) as connection:
-        client = Client(connection)    
+        client = Client(connection)
         count = 1
         lastchanged={}
         lastvalue={}
@@ -117,7 +121,7 @@ def main():
         not_stale_count = 0
         reconnect_count = 0
         zapacket = []
-        while( True ):
+        while True:
             api_endpoint = 'device.signal' #hack until able to parse api endpoints and keys from yaml config
             try:
                 stuff = client.device.signal()
@@ -192,7 +196,7 @@ def main():
                     # if sending fails, zabbix server may be restarting or server rebooting.
                     # maybe it gets sent after the next polling attempt.
                     # if zapacket is more than x items or if failure time greater than x) save to disk
-                    
+
                     if zabbix_send_failed_time == 0 : #first fail (after successfully sending or saving)
                         zabbix_send_failed_time = epoch_time
                         # could set defaults for failed item sending timeout and count.
@@ -208,7 +212,6 @@ def main():
                 except:
                     logging.error("Unexpected error:", sys.exc_info()[0])
                     raise
-                    
             else:
                 print('***** TEST: not sending *****')
                 pprint.pp(zapacket )
