2a42b6428e16af9fd5c6fae9613b649a74e0062a
Mike G <mkgin@users.noreply.github.com>
2022-08-30 23:26:45 +0300

    PYLINT now 5.16/10

--- huawei_lte_api_zabbix_sender.py
+++ huawei_lte_api_zabbix_sender.py
@@ -1,15 +1,32 @@
-#
-# Huawei LTE modem monitor for Zabbix
-#
-#
-# - monitor, store last and only report changes for some parameters send data to Zabbix.
-# - collect timestamps
-#
-# - for some items, send timestamp , mark what was last sent and send when
-# - tested with a 4G-LTE modem ( Huawei B535-232) so I may have missed some parameters
-#   the would be interesting to 5G (or 3G)
-#
+"""  Huawei LTE modem monitor for Zabbix
 
+https://github.com/mkgin/huawei_LTE_API_Zabbix_Sender
+
+This script monitors a compatible Huawei Modem (4G, LTE, 5G) via the
+huawei_lte_api module and reports values to a Zabbix server using the
+py-zabbix module.
+
+Items to be monitored are configured in a YAML configuration file. Items
+can be sent to the server each time polled, at defined intervals or
+whem a change in value is detected. For a given API endpoint, all items
+are polled at or near the the shortest reporting rate in that endpoint.
+If unchanged, items with longer reporting rates when a change is observed
+or when the time since last report sent is exceeded. When a change is observed,
+for such an item, the value (unchanged) from the previous polling is sent
+to the server as well as the current value.
+
+In the event of a brief connection errors to the Zabbix server, the retrieved
+items (and timestamps) are queued to be reported when the connection is again
+possible). For longer interruptions, the results can be stored to a file and
+manually updated later.
+"""
+
+import pprint
+import time
+import sys  # to report errors
+import logging
+import os
+import socket #for errors from ZabbixSender
 from huawei_lte_api.Client import Client #https://github.com/Salamek/huawei-lte-api
 from huawei_lte_api.Connection import Connection
 from huawei_lte_api.exceptions import \
@@ -20,14 +37,11 @@ from huawei_lte_api.exceptions import \
     ResponseErrorLoginCsrfException, \
     ResponseErrorWrongSessionToken, \
     RequestFormatException
-from pyzabbix import ZabbixMetric, ZabbixSender, ZabbixResponse  #https://py-zabbix.readthedocs.io/en/latest/sender.html
-import pprint
-import time
-import sys  # to report errors
-import logging
-import os
+from pyzabbix import ZabbixMetric, ZabbixSender, ZabbixResponse
+# https://py-zabbix.readthedocs.io/en/latest/sender.html
+# ZabbixResponse might not be needed
 import yaml
-import socket #for errors from ZabbixSender
+
 
 log_level =''
 zabbix_sender_setting = ''
@@ -37,6 +51,7 @@ minimum_polling_interval = ''
 
 
 def load_config():
+    """Loads the YAML config first from config.yml, then own_config.yml"""
     # basic config
     # api config
     config = {}
@@ -72,6 +87,7 @@ def load_config():
     zabbix_send_failed_items_max = 500 # TODO add to default config
 
 def save_zabbix_packet_to_disk(object):
+    """Saves unsent items to Disk"""
     logging.warning(f'TEST: save_zabbix_packet_to_disk: NOT IMPLEMENTED YET' )
 
 #todo
@@ -95,7 +111,6 @@ interesting = always_interesting +  changes_interesting_10min + changes_interest
 
 # will be determined from the smallest interval in the API configuration
 polling_interval = 60
-
 api_config = {} #TODO
 
 def main():
@@ -108,10 +123,12 @@ def main():
     zabbix_send_failed_time = 0
     with Connection(modem_url) as connection:
         client = Client(connection)
-        count = 1
-        lastchanged={}
-        lastvalue={}
+        lastchanged = {}
+        lastvalue = {}
+        #timers
+        epoch_time_last = 0
         #counters
+        count = 1
         zabbix_server_processed = 0
         zabbix_server_failed = 0
         zabbix_server_total = 0
@@ -125,7 +142,8 @@ def main():
             api_endpoint = 'device.signal' #hack until able to parse api endpoints and keys from yaml config
             try:
                 stuff = client.device.signal()
-            except ( ResponseErrorException, ResponseErrorLoginCsrfException, ResponseErrorLoginRequiredException ) as error_msg:
+            except ( ResponseErrorException, ResponseErrorLoginCsrfException,\
+                    ResponseErrorLoginRequiredException ) as error_msg:
                 logging.warning('Reconnecting due to error: {0}'.format(error_msg))
                 reconnect_count += 1
                 logging.warning(f'Reconnect count: {reconnect_count}' )
@@ -137,17 +155,19 @@ def main():
                 logging.warning('Too many failed logins to modem: {0}'.format(error_msg))
                 logging.warning('Sleeping one minute before trying again')
             except:
-                logging.error("Unexpected error:", sys.exc_info()[0])
+                logging.error('Unexpected error: {0}' .sys.exc_info()[0])
                 raise
             # handle error... renew connection?
             epoch_time = int(time.time())
             for k,v in stuff.items():
                 # if k in parsed, parse it  TODO (make a new function for it)
                 if k in always_interesting:
-                    zapacket = zapacket + [ ZabbixMetric( monitored_hostname , f'{key_prefix}.{api_endpoint}.{k}' , v, epoch_time ) ]
+                    zapacket = zapacket + [ ZabbixMetric( monitored_hostname,
+                        f'{key_prefix}.{api_endpoint}.{k}' , v, epoch_time ) ]
                 elif ( k in interesting ) and (k not in lastchanged): #or lastvalue...
                     logging.debug(f"{k} not in lastchanged {k not in lastchanged}")
-                    zapacket = zapacket + [ ZabbixMetric( monitored_hostname , f'{key_prefix}.{api_endpoint}.{k}' , v, epoch_time ) ]
+                    zapacket = zapacket + [ ZabbixMetric( monitored_hostname,
+                        f'{key_prefix}.{api_endpoint}.{k}' , v, epoch_time ) ]
                     lastchanged[k] = epoch_time
                     lastvalue[k] = v
                 elif k in interesting: #changes_interesting_10min or k in changes_interesting_1hour:
@@ -156,19 +176,22 @@ def main():
                          or (k in changes_interesting_10min and (epoch_time - lastchanged[k] > one_hour ))
                          or (k in changes_interesting_1hour and (epoch_time - lastchanged[k] > ten_minutes ))
                         ):
-                        # if value changed. send previous data from previous interval with last interval if not equal.
+                        # if value changed. send previous data from previous
+                        #interval with last interval if not equal.
                         if not lastvalue[k] == v:
                             logging.debug(f'{k} changed from {lastvalue[k]} to {v}')
                             changed_count += 1
                             not_stale_count += 1
                             if epoch_time_last > lastchanged[k]:
-                                zapacket = zapacket + [ ZabbixMetric( monitored_hostname , f'{key_prefix}.{api_endpoint}.{k}' ,lastvalue[k] ,epoch_time_last) ]
+                                zapacket = zapacket + [ ZabbixMetric( monitored_hostname ,
+                                    f'{key_prefix}.{api_endpoint}.{k}' ,lastvalue[k] ,epoch_time_last) ]
                                 logging.debug(f'{k} changed before last check sending previous interval sending previous data from {epoch_time - lastchanged[k]} sec')
                         else:
                             logging.debug(f'{k} data is stale after {epoch_time - lastchanged[k]} sec')
                             stale_count += 1
                             not_changed_count += 1
-                        zapacket = zapacket + [ ZabbixMetric( monitored_hostname , f'{key_prefix}.{api_endpoint}.{k}' , v, epoch_time ) ]
+                        zapacket = zapacket + [ ZabbixMetric( monitored_hostname ,
+                            f'{key_prefix}.{api_endpoint}.{k}' , v, epoch_time ) ]
                         lastchanged[k] = epoch_time
                         lastvalue[k] = v
                     else:
@@ -176,7 +199,6 @@ def main():
                         not_stale_count += 1
                         logging.debug(f'{k} not changed/not stale or too old {epoch_time - lastchanged[k]} ')
                 else:
-                    z=1
                     logging.debug(f'{k} is not interesting: {k} : {v}')
 
             epoch_time_last = epoch_time
@@ -193,11 +215,11 @@ def main():
                     logging.debug(f'Zabbix Sender succeeded\n{zaserver_response}')
                 except (socket.timeout, socket.error, ConnectionRefusedError ) as error_msg:
                     logging.warning('Zabbix Sender Failed to send some or all: {0}'.format(error_msg))
-                    # if sending fails, zabbix server may be restarting or server rebooting.
+                    # if sending fails, Zabbix server may be restarting or rebooting.
                     # maybe it gets sent after the next polling attempt.
-                    # if zapacket is more than x items or if failure time greater than x) save to disk
-
-                    if zabbix_send_failed_time == 0 : #first fail (after successfully sending or saving)
+                    # if zapacket is more than x items or if failure time
+                    # greater than x save to disk
+                    if zabbix_send_failed_time == 0 : #first fail (after successful send or save)
                         zabbix_send_failed_time = epoch_time
                         # could set defaults for failed item sending timeout and count.
                     if  ( epoch_time - zabbix_send_failed_time > zabbix_send_failed_time_max #900
@@ -205,20 +227,23 @@ def main():
                         ):
                         logging.error(f'Zabbix Sender failed {epoch_time - zabbix_send_failed_time} seconds ago and {len(zapacket)} items pending.\nWill try to dump them to disk')
                         save_zabbix_packet_to_disk(zapacket)
-                        zabbix_send_failed_time = epoch_time #
+                        zabbix_send_failed_time = epoch_time
                         zapacket = [] #it's saved now ok to erase
-                        # clear keys from lastchanged so fresh values are collected to be sent next time
+                        # clear keys from lastchanged so are collected to be sent next time
                         lastchanged={}
                 except:
-                    logging.error("Unexpected error:", sys.exc_info()[0])
+                    logging.error(f'Unexpected error: {sys.exc_info()[0]}')
                     raise
             else:
                 print('***** TEST: not sending *****')
                 pprint.pp(zapacket )
                 zapacket = []
-            print(f'*** Time: {epoch_time} poll count: {count} uptime: {epoch_time - epoch_time_start} ***')
-            print(f'*** stale:not {stale_count}:{not_stale_count} , changed:not {changed_count}:{not_changed_count} ***')
-            print(f'*** zabbix_server totals, processed: {zabbix_server_processed} failed: {zabbix_server_failed} total: {zabbix_server_total} ***')
+            print(f'*** Time: {epoch_time} poll count: {count}',
+                  f' uptime: {epoch_time - epoch_time_start} ***')
+            print(f'*** stale:not {stale_count}:{not_stale_count},',
+                  f' changed:not {changed_count}:{not_changed_count} ***')
+            print(f'*** zabbix_server processed: {zabbix_server_processed},',
+                  f' failed: {zabbix_server_failed}, total: {zabbix_server_total} ***')
             count += 1
             if not do_it_once:
                 time.sleep(polling_interval)
